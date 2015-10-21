bec0af207febb1e957b29eccd2101d8086aee00d
rivaille-c <rivaille-c@mgdis.fr>
2015-10-21 14:42:13 +0200

    fork build

--- dist/adf-widget-iframe.js
+++ dist/adf-widget-iframe.js
@@ -18,11 +18,12 @@ angular.module('adf.widget.iframe', ['adf.provider'])
         }
       });
   }])
-  .controller('iframeController', ["$sce", "config", function($sce, config){
+  .controller('iframeController', ["$scope", "$sce", "config", function($scope, $sce, config){
+    $scope.$emit('widgetReady', $scope.config);
     if (config.url){
       this.url = $sce.trustAsResourceUrl(config.url);
     }
   }]);
 
-angular.module("adf.widget.iframe").run(["$templateCache", function($templateCache) {$templateCache.put("{widgetsPath}/iframe/src/edit.html","<form role=form><div class=form-group><label for=url>URL</label> <input type=url class=form-control id=url ng-model=config.url placeholder=http://www.example.com></div><div class=form-group><label for=url>Height</label> <input type=text class=form-control id=url ng-model=config.height></div></form>");
-$templateCache.put("{widgetsPath}/iframe/src/view.html","<div><div class=\"alert alert-info\" ng-if=!config.url>Please insert a url in the widget configuration</div><iframe ng-if=iframe.url class=adf-iframe style=\"height: {{config.height}}\" src={{iframe.url}}></iframe></div>");}]);})(window);
+angular.module("adf.widget.iframe").run(["$templateCache", function($templateCache) {$templateCache.put("{widgetsPath}/iframe/src/edit.html","<form role=form><div class=form-group><label for=url>{{config.namespace + \'.iframe.url\' | translate}}</label> <input type=url class=form-control id=url ng-model=config.url placeholder=\"{{config.namespace + \'.iframe.placeholder\' | translate}}\"></div><div class=form-group><label for=url>{{config.namespace + \'.iframe.height\' | translate}}</label> <input type=text class=form-control id=url ng-model=config.height></div></form>");
+$templateCache.put("{widgetsPath}/iframe/src/view.html","<div><div class=\"alert alert-info\" ng-if=!config.url>{{config.namespace + \'.iframe.noUrl\' | translate}}</div><iframe ng-if=iframe.url class=adf-iframe style=\"height: {{config.height}}\" ng-src={{iframe.url}}></iframe></div>");}]);})(window);
